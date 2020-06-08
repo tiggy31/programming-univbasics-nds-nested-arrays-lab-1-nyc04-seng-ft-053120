d6d564fcbeff0a4c0794c48cdd3a0ba2fc2737f5
TIGGY ALEMU <tigist_alemu@fitnyc.edu>
2020-06-08 17:50:31 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -54,8 +54,8 @@ def matrix_lookup(matrix, row, column)
 end
 
 def matrix_update(matrix, row, column, new_value)
-  binding.pry
-matrix[0] = new_value
+  #binding.pry
+matrix[row][column] = new_value
  
   # Given any matrix (array of arrays), a row index and a column index, 
   # Update the matrix location at that row and column to have the value of new_value
