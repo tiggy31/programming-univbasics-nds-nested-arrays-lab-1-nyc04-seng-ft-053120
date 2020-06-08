95db572f38ea60d624523a27b7ab2cb33c9a942b
TIGGY ALEMU <tigist_alemu@fitnyc.edu>
2020-06-08 17:35:02 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -55,7 +55,7 @@ end
 
 def matrix_update(matrix, row, column, new_value)
   #binding.pry
-  new_value = matrix[1][0] "onion"
+  new_value = matrix[1][0] 
   matrix
   # Given any matrix (array of arrays), a row index and a column index, 
   # Update the matrix location at that row and column to have the value of new_value
